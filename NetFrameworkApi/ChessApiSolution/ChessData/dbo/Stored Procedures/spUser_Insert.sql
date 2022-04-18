50f45d457685595aec16cdcd6afb015621154550
KyleCrawford <kyle.g.crawford@gmail.com>
2022-04-18 15:55:52 -0600

    Insert User stored procedure
    Insert User stored procedure

---
+++ NetFrameworkApi/ChessApiSolution/ChessData/dbo/Stored Procedures/spUser_Insert.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Insert]
+	@Id NVARCHAR(128),
+	@Username NVARCHAR(50),
+	@Email NVARCHAR(256),
+	@CreatedDate DATETIME2
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO dbo.[User](Id, Username, Email, CreatedDate)
+	VALUES (@Id, @Username, @Email, @CreatedDate)
+END
