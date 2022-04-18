2c905d6125d51435b01167e30902b6371faba39d
KyleCrawford <kyle.g.crawford@gmail.com>
2022-04-17 22:07:44 -0600

    Internal DataAccess class
    Internal DataAccess class

---
+++ NetFrameworkApi/ChessApiSolution/DataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        // functions needed:
+            // get logged in user
+            // update info
+            // delete account
+            // create? Going to need it, don't know how this is going to work just yet
+    }
+}
