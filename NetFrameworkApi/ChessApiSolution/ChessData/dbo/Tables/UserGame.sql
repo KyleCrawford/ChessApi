1f1e02749565fcd6bba109efd889ff5385fdbc28
KyleCrawford <kyle.g.crawford@gmail.com>
2022-04-17 21:36:03 -0600

    UserGame Table
    UserGame Table
    Changed User Id to nvarchar(128)

---
+++ NetFrameworkApi/ChessApiSolution/ChessData/dbo/Tables/UserGame.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[UserGame]
+(
+	[GameId] INT NOT NULL PRIMARY KEY,
+	[PlayerOne] NVARCHAR(128) NOT NULL,
+	[PlayerTwo] NVARCHAR(128) NOT NULL,
+
+	CONSTRAINT [FK_UserGame_ToGame] FOREIGN KEY (GameId) REFERENCES Game(Id),
+	CONSTRAINT [FK_UserGame_ToUserOne] FOREIGN KEY (PlayerOne) REFERENCES [User](Id),
+	CONSTRAINT [FK_UserGame_ToUserTwo] FOREIGN KEY (PlayerTwo) REFERENCES [User](Id)
+)
