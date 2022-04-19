a7cabcb165ba70a1c0e78ac6ae88fd390b70b28e
KyleCrawford <kyle.g.crawford@gmail.com>
2022-04-18 22:05:57 -0600

    Game Controller Template Done
    Game Controller Template Done

--- NetFrameworkApi/ChessApiSolution/ChessApi/Controllers/GameController.cs
+++ NetFrameworkApi/ChessApiSolution/ChessApi/Controllers/GameController.cs
@@ -12,96 +12,11 @@ namespace ChessApi.Controllers
 	//[Authorize]
 	public class GameController : ApiController
     {
-		/*
-         * // User has ability to create game, join game, invite player to game
-	
-	Create Game
-		// This will create a game with the current player
-		[post]
-		/api/game/create
-	
-	Join Game
-		// Adds the logged in user to the provided game (if possible)
-		[get?]
-		/api/game/join/id
-	
-	Invite player 
-		// Sends an invite to another player
-		[get?]
-		/api/game/invite/id
-	
-	Update Board
-		// updates the chess board
-		[put]
-		/api/game/updateBoard
-	
-	Leave game?
-		// Is needed, does it need to be an api call?
-		// yes
-		[put]
-		/api/game/leave/id
-	
-	// Delete game
-		// can be deleted by either player
-		// or
-		// can be deleted by maker, and if there is only 1 player, they can delete it if owner or not
-		[delete]
-		/api/game/delete/id
-
-
-
-		// trying to combine routes
-
-		create game
-			[post]
-			/api/game
-		
-		delete game
-			[delete]
-			/api/game/id
-		
-		Get Game Info
-			[get]
-			/api/game/id
-		
-		update board (update game table)
-			[put]
-			/api/game/id
-			
-		
-		// leave game (remove player)
-			[put]
-			/api/game/leave/gameid
-		
-
-		/api/game/invite/gameid
-
-		post?
-		// invite player (send invitation to another user)
-			do we do a post?
-			/api/game/invite/gameid? - posts { "id": "userid" }
-			// Makes the most sense to do a post
-
-		put?
-		// join game ( add player)
-			/api/game/invite/gameid - puts { "id": "userid" }
-
-         * */
-
-
-
-		/*
-		[Authorize(Roles = "Admin")]
-        [HttpGet]
-        [Route("api/User/Admin/GetAllUsers")]
-		// We do not need api/User because we set it at the top
-		 * */
-
 
 		// GET: api/Game/5
 		[HttpGet]
 		[Route("api/Game/{id}")]
-		public string ChangedName(int id)
+		public string GetUserDetails(int id)
         {
             return id + "value";
         }
@@ -109,15 +24,15 @@ namespace ChessApi.Controllers
 		// POST: api/Game
 		[HttpPost]
 		[Route("api/Game")]
-		public string CreateGame(TestModel value)
+		public string CreateGame([FromBody] Dictionary<string,string> value)
         {
 			var test = value;
-			return "Hey we're here";
+			return "Hey we're here " + value;
 		}
 
 		// PUT: api/Game/5
 		[HttpPut]
-		[Route("api/Game")]
+		[Route("api/Game/{id}")]
 		public void UpdateBoard(int id, [FromBody]string value)
         {
 
@@ -125,37 +40,30 @@ namespace ChessApi.Controllers
 
 		// DELETE: api/Game/5
 		[HttpDelete]
-		[Route("api/Game")]
+		[Route("api/Game/{id}")]
 		public void Delete(int id)
         {
 
         }
 
-
-		/*
-		 * 
-		 * // leave game (remove player)
-			[put]
-			/api/game/leave/gameid
-		
-
-		/api/game/invite/gameid
-
-		post?
-		// invite player (send invitation to another user)
-			do we do a post?
-			/api/game/invite/gameid? - posts { "id": "userid" }
-			// Makes the most sense to do a post
-
-		put?
-		// join game ( add player)
-			/api/game/invite/gameid - puts { "id": "userid" }
-		 * */
-
 		// PUT: api/Game/5
 		[HttpPut]
-		[Route("api/Game/Leave")]
+		[Route("api/Game/Leave/{id}")]
 		public void LeaveGame(int id, [FromBody] string value)
+		{
+
+		}
+
+		[HttpPost]
+		[Route("api/Game/Invite/{id}")]
+		public void InviteToGame(int id, [FromBody] string value)
+		{
+
+		}
+
+		[HttpPut]
+		[Route("api/Game/Invite/{id}")]
+		public void JoinGame(int id, [FromBody] string value)
 		{
 
 		}
