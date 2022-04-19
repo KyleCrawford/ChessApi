1d436dfd5d4eb663f8958d99cc36edb9e2d7723a
KyleCrawford <kyle.g.crawford@gmail.com>
2022-04-18 23:22:40 -0600

    Create Game Stored Proc
    Create Game Stored Proc

---
+++ NetFrameworkApi/ChessApiSolution/ChessData/dbo/Stored Procedures/spCreateGame.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[spCreateGame]
+	@Name NVARCHAR(100),
+	@Code NVARCHAR(50),
+	@BoardData NVARCHAR(MAX),
+	@UserId NVARCHAR(128)
+AS
+BEGIN TRANSACTION
+BEGIN
+	SET NOCOUNT ON;
+
+	-- Create the new game record
+	INSERT INTO Game ([Name], Code, BoardData)
+	VALUES (@Name, @Code, @BoardData)
+	
+	if @@ERROR <> 0
+	BEGIN
+		ROLLBACK TRANSACTION
+		RETURN 2
+	END
+
+	-- Link the User and Game
+	INSERT INTO UserGame (GameId, PlayerOne)
+	VALUES (SCOPE_IDENTITY(), @UserId)
+
+	if @@ERROR <> 0
+	BEGIN
+		ROLLBACK TRANSACTION
+		RETURN 4
+	END
+
+END
